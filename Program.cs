4fc740aeedbacb8104dd9606b07e48740e9e6ac4
Andrii Banduliak <banduliak1303@gmail.com>
2022-08-07 14:07:18 +0300

    2nd task is done

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Enter the number one ");
+int num1 = int.Parse(Console.ReadLine());
+
+Console.Write("Enter the number two ");
+int num2 = int.Parse(Console.ReadLine());
+
+Console.Write("Enter the number three ");
+int num3 = int.Parse(Console.ReadLine());
+
+if(num1 > num2 && num1 >num3)
+Console.WriteLine($" The biggest number is {num1} ");
+if(num2>num1 && num2>num3)
+Console.WriteLine($"The biggest number is {num2} ");
+else
+Console.Write($"The biggest number is {num3}");
